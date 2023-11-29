5d6c65e6260ce74f71ed6303be2668e4253a72db
ravi hennayake <ravihennayaka@outlook.com>
2023-11-29 16:23:44 +0530

    NavBar develope

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+ // assuming Navbar is in the same directory
+import Dashboard from "./Pages/Dashboard";
+import AddInvoice from "./Pages/Invoice/AddInvoice";
+import ViewInvoice from "./Pages/Invoice/InvoiceView";
+import AddCustomers from "./Pages/Customers/addCustomers";
+import ViewCustomers from "./Pages/Customers/viewCustomers";
+import AddGRN from "./Pages/GRN/GrnAdd";
+import ViewGRN from "./Pages/GRN/ViewGRN";
+import Login from "./Components/Login";
+
 import Navbar from './Components/Navbar'
 
 function App() {
  
 
   return (
-    <>
-<Navbar/>
-    </>
+    <Router>
+     
+     
+        <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Navbar><Dashboard /></Navbar>} />
+        <Route path="/invoiceAdd" element={<Navbar><AddInvoice /></Navbar>} />
+        <Route path="/invoiceView" element={<Navbar><ViewInvoice /></Navbar>} />
+        <Route path="/addCustomers" element={<Navbar><AddCustomers /></Navbar>} />
+        <Route path="/viewCustomers" element={<Navbar><ViewCustomers /></Navbar>} />
+        <Route path="/addGRN" element={<Navbar><AddGRN /></Navbar>} />
+        <Route path="/ViewGRN" element={<Navbar><ViewGRN /></Navbar>} />
+
+        </Routes>
+     
+    </Router>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,7 +18,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
 import { Button } from "@mui/material";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 // import { BrowserRouter as Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -27,21 +27,21 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
 import "../assets/Navbar.css";
 
 // Side Bar Section Imports
-import Dashboard from "../Pages/Dashboard";
+
 import SalesSection from "./SideBarSections/SalesSection";
 import InventorySection from "./SideBarSections/InventorySection";
 import BReportSection from "./SideBarSections/B_ReportSection";
 import GrnSection from "./SideBarSections/GrnSection";
 import AnalyticsSection from "./SideBarSections/AnalyticsSection";
 import AdminSection from "./SideBarSections/AdminSection";
-import AddInvoice from "../Pages/Invoice/AddInvoice";
+// import AddInvoice from "../Pages/Invoice/AddInvoice";
 
-import ViewInvoice from "../Pages/Invoice/InvoiceView";
-import AddCustomers from "../Pages/Customers/addCustomers";
-import ViewCustomers from "../Pages/Customers/viewCustomers";
-import AddGRN from "../Pages/GRN/GrnAdd";
-import ViewGRN from "../Pages/GRN/ViewGRN";
-import Login from "../Components/Login";
+// import ViewInvoice from "../Pages/Invoice/InvoiceView";
+// import AddCustomers from "../Pages/Customers/addCustomers";
+// import ViewCustomers from "../Pages/Customers/viewCustomers";
+// import AddGRN from "../Pages/GRN/GrnAdd";
+// import ViewGRN from "../Pages/GRN/ViewGRN";
+// import Login from "../Components/Login";
 
 
 // import SignUp from "../Components/SignUp";
@@ -50,176 +50,161 @@ import Login from "../Components/Login";
 
 const drawerWidth = 240;
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
-  })
+    ({ theme, open }) => ({
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: `-${drawerWidth}px`,
+        ...(open && {
+            transition: theme.transitions.create("margin", {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        }),
+    })
 );
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+    shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
     transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
     }),
-  }),
+    ...(open && {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(["margin", "width"], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
 }));
 
 const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
 }));
 
-export default function Navbar() {
-  const theme = useTheme();
+export default function Navbar({children}) {
+    const theme = useTheme();
 
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <>
-      <body backgroundColor="#AAB7B8">
-        <Router>
-        <Box sx={{ display: "flex" }}>
-          <CssBaseline />
-          <AppBar
-            position="fixed"
-            className="NavBarColor"
-            open={open}
-            style={{ backgroundColor: "#011f4b  " }}
-          >
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
-                sx={{ mr: 2, ...(open && { display: "none" }) }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography
-                variant="h6"
-                noWrap
-                color={"#b3cde0"}
-                component="div"
-              >
-                BIZ-Pilot
-              </Typography>
-              <Button variant="outlined" style={{ display: 'flex', justifyContent: 'right' }}>Primary</Button>
-            </Toolbar>
-
-
+    return (
+        <>
+            <body backgroundColor="#AAB7B8">
+        
+                    <Box sx={{ display: "flex" }}>
+                        <CssBaseline />
+                        <AppBar
+                            position="fixed"
+                            className="NavBarColor"
+                            open={open}
+                            style={{ backgroundColor: "#011f4b  " }}
+                        >
+                            <Toolbar>
+                                <IconButton
+                                    color="inherit"
+                                    aria-label="open drawer"
+                                    onClick={handleDrawerOpen}
+                                    edge="start"
+                                    sx={{ mr: 2, ...(open && { display: "none" }) }}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Typography
+                                    variant="h6"
+                                    noWrap
+                                    color={"#b3cde0"}
+                                    component="div"
+                                >
+                                    BIZ-Pilot
+                                </Typography>
+                                <Button variant="outlined" style={{ display: 'flex', justifyContent: 'right' }}>Primary</Button>
+                            </Toolbar>
 
 
-          </AppBar>
-          <Drawer
-            sx={{
-              width: drawerWidth,
-              flexShrink: 0,
-              "& .MuiDrawer-paper": {
-                width: drawerWidth,
-                boxSizing: "border-box",
-              },
-            }}
-            variant="persistent"
-            anchor="left"
-            open={open}
-            color="white"
-          >
-            <DrawerHeader sx={{ background: "#011f4b" }}>
-              <IconButton onClick={handleDrawerClose} color="white">
-                {theme.direction === "ltr" ? (
-                  <ChevronLeftIcon />
-                ) : (
-                  <ChevronRightIcon />
-                )}
-              </IconButton>
 
-            </DrawerHeader>
-            <Divider />
 
-            <List>
-              <ListItem disablePadding>
-                {/* <ListItemButton
+                        </AppBar>
+                        <Drawer
+                            sx={{
+                                width: drawerWidth,
+                                flexShrink: 0,
+                                "& .MuiDrawer-paper": {
+                                    width: drawerWidth,
+                                    boxSizing: "border-box",
+                                },
+                            }}
+                            variant="persistent"
+                            anchor="left"
+                            open={open}
+                            color="white"
+                        >
+                            <DrawerHeader sx={{ background: "#011f4b" }}>
+                                <IconButton onClick={handleDrawerClose} color="white">
+                                    {theme.direction === "ltr" ? (
+                                        <ChevronLeftIcon />
+                                    ) : (
+                                        <ChevronRightIcon />
+                                    )}
+                                </IconButton>
+
+                            </DrawerHeader>
+                            <Divider />
+
+                            <List>
+                                <ListItem disablePadding>
+                                    {/* <ListItemButton
                     component={Link}
                     to="/dashboard"
                     className="list-item"
                     id="ListItmBtn"
                   > */}
 
-                <ListItemButton
-                  component={Link}
-                  to="/login"
-                  className="list-item"
-                  id="ListItmBtn"
-                >
-                  <ListItemIcon>
-                    <FontAwesomeIcon id="icon" icon={faHome} size="lg" />
-                  </ListItemIcon>
-                  <ListItemText primary="Dashboard" />
-                </ListItemButton>
-              </ListItem>
+                                    <ListItemButton
+                                        component={Link}
+                                        to="/login"
+                                        className="list-item"
+                                        id="ListItmBtn"
+                                    >
+                                        <ListItemIcon>
+                                            <FontAwesomeIcon id="icon" icon={faHome} size="lg" />
+                                        </ListItemIcon>
+                                        <ListItemText primary="Dashboard" />
+                                    </ListItemButton>
+                                </ListItem>
 
-              <SalesSection />
-              <InventorySection />
-              <BReportSection />
-              <GrnSection />
-              <AnalyticsSection />
-              <AdminSection />
-            </List>
-          </Drawer>
-          <Main open={open}>
-            <DrawerHeader />
-
-
-
-
-            <Routes>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/invoiceAdd" element={<AddInvoice />} />
-                <Route path="/invoiceView" element={<ViewInvoice />} />
-                <Route path="/addCustomers" element={<AddCustomers />} />
-                <Route path="/viewCustomers" element={<ViewCustomers />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/addGRN" element={<AddGRN />} />
-                <Route path="/ViewGRN" element={<ViewGRN />} />
-
-              </Routes>
-          </Main>
-        </Box>
-        </Router>
-      </body>
-    </>
-  );
+                                <SalesSection />
+                                <InventorySection />
+                                <BReportSection />
+                                <GrnSection />
+                                <AnalyticsSection />
+                                <AdminSection />
+                            </List>
+                        </Drawer>
+                        <Main open={open}>
+                            <DrawerHeader />
+                            {children}
+                        </Main>
+                    </Box>
+            </body>
+        </>
+    );
 }
