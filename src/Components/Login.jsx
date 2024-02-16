91ca84b36a1286a0d7ce7d2338cd0cfce29bbe00
ravi hennayake <ravihennayaka667@gmail.com>
2024-02-16 22:16:05 +0530

    login ui done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,107 +7,116 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import '../assets/Login.css';
-import { Card, CardContent, CardHeader } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { styled } from '@mui/system';
+import '../assets/Login.css'; // Assuming you have a CSS file for additional styling
 
-const defaultTheme = createTheme();
+const customTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#3c096c", // Change to your desired primary color
+        },
+        secondary: {
+            main: "#000fds", // Change to your desired secondary color
+        },
+    },
+});
+
+const LoginCard = styled(Card)({
+    maxWidth: 400,
+    margin: 'auto',
+    marginTop: 8,
+    padding: 2,
+});
+
+const LoginCardContent = styled(CardContent)({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+});
 
 export default function Login() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get("email"),
+            password: data.get("password"),
+        });
+    };
 
-  return (
-    <>
-      <div className="login">
-      <Grid container justifyContent="center">
-      <Grid item xs={12} sm={8} md={6} lg={4}>
-      <Card id="bglogin">
-              <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                  sx={{
-                    marginTop: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <Avatar sx={{ m: 1, bgcolor: "#03396c" }}>
-                    <LockOutlinedIcon />
-                  </Avatar>
-                  <Typography component="h1" variant="h5" style={{ color: 'black' }}>
-                    Sign in
-                  </Typography>
-                  <CardContent>
-                    <Box
-                      component="form"
-                      onSubmit={handleSubmit}
-                      noValidate
-                      sx={{ mt: 1 }}
-                    >
-                      <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                      />
-                      <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                      />
-                      <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                      />
-                      <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                      >
-                        Sign In
-                      </Button>
-                      <Grid container>
-                        <Grid item xs>
-                          <Link href="#" variant="body2">
-                            Forgot password?
-                          </Link>
+    return (
+        <ThemeProvider theme={customTheme}>
+            <CssBaseline />
+            <Container component="main" maxWidth="xs" >
+                <LoginCard>
+                    <CardHeader
+                        avatar={
+                            <Avatar sx={{ bgcolor: "primary.main" }}>
+                                <LockOutlinedIcon />
+                            </Avatar>
+                        }
+                        title={<Typography variant="h5">Sign in</Typography>}
+                    />
+                    <LoginCardContent>
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </Button>
+                        </form>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
                         </Grid>
-                        <Grid item xs>
-                          <Link href="#" variant="body2">
-                            {"Don't have an account? Sign Up"}
-                          </Link>
-                        </Grid>
-                      </Grid>
-                    </Box>
-                    </CardContent>
-                </Box>
-              </Container>
-            </Card>
-          </Grid>
-        </Grid>
-      </div>
-        </>
-        );
+                    </LoginCardContent>
+                </LoginCard>
+            </Container>
+        </ThemeProvider>
+    );
 }
